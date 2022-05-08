18b73ff4c7e3b6c09326d322027b92b604f927eb
Aditya <aditya080102@gmail.com>
2022-05-08 18:41:43 +0530

    First commit

--- app.js
+++ app.js
@@ -118,7 +118,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Serving on port ${port}`)
 })
